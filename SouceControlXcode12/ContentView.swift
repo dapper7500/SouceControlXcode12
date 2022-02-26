c6ffe90ea06d6e21592eb3cd0fb6fa622ce0aaf7
Scott Gainey <gaineys@me.com>
2022-02-25 21:20:29 -0600

    Initial Commit

---
+++ SouceControlXcode12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SouceControlXcode12
+//
+//  Created by Scott Gainey on 2/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SouceControlXcode12/SouceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SouceControlXcode12App.swift
+//  SouceControlXcode12
+//
+//  Created by Scott Gainey on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SouceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
